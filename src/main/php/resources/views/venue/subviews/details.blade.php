e692542a0a57c8c08de839822777a322c0c37736
Ben <ben2117@me.com>
2017-08-28 16:00:44 +1000

    add future events

--- src/main/php/resources/views/venue/subviews/details.blade.php
+++ src/main/php/resources/views/venue/subviews/details.blade.php
@@ -8,4 +8,21 @@
       <p> Contact : <b> (02) 9003 3820</b></p>
       <p> max Capacity : <b> 5 people </b></p>
     </ul>
+    <ul class="nav nav-pills nav-stacked">
+    <hr>
+    <p><b> Future Events </b></p> 
+     <ul class="nav nav-pills nav-stacked">                 
+    <li role="presentation">
+        <div class="row">
+            <a href="#">
+                <div class=" col-xs-4 "> Going To The Movies</div>
+                <div class="col-xs-8 text-right">28th Sep 04:00 am
+                    <span class="ml-1"><button class="sl-2 btn btn-primary btn-xs">Guests <span class="badge" naevent-guest-nb">{{sprintf('%02d', rand(0,5))}}</span></button></span>
+                </div>
+            </a> 
+        </div>
+    </li>
+    </ul>
+
+
 </div>
